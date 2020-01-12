022cdb56901bfd1d8a52cdd55b1ad01079a6216d
horeeorg <horee.org@gmail.com>
2020-01-12 19:03:31 +0700

    Fixing bug

--- bintangsegitiga.dart
+++ bintangsegitiga.dart
@@ -7,30 +7,24 @@ main()
   print("Input Number: ");
   String userInput = stdin.readLineSync();
 
-  try {
+  inputNumber = int.tryParse(userInput) ?? 0; 
 
-    inputNumber = int.tryParse(userInput); 
+  print('Start');
+  if (inputNumber <= 0) 
+    print('You must input a number or your number should greater than 0');
 
-    print('Start');
-    if (inputNumber == 0) 
-      print('Input harus lebih besar 0');
-
-    int helper = 0;
-    for(int lineCount=0; lineCount < inputNumber; lineCount++) {
-      
-      for(int spaceCount=0; spaceCount < inputNumber - lineCount; spaceCount++) {
-        stdout.write(' ');
-      }
-
-      helper++;
-      for(int starCount=0; starCount < helper + lineCount; starCount++){
-        stdout.write('*');
-      }
-      
-      stdout.write('\n');
+  int helper = 0;
+  for(int lineCount=0; lineCount < inputNumber; lineCount++) {
+    
+    for(int spaceCount=0; spaceCount < inputNumber - lineCount; spaceCount++) {
+      stdout.write(' ');
     }
-  }
-  on FormatException {
-    print ('Your input should be number');
+
+    helper++;
+    for(int starCount=0; starCount < helper + lineCount; starCount++){
+      stdout.write('*');
+    }
+    
+    stdout.write('\n');
   }
 }
